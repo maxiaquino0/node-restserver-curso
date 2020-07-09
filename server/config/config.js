86446025123d954be33878cfd3bbe76722932144
Maxi Aquino <maxiaquino0@gmail.com>
2020-07-09 19:51:01 -0300

    RESTSERVER - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,16 @@
 // Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+
+// Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+// Base de datos
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://maxiaquino0:g3HMz4A9asAz6tm4@cluster0.zkypo.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
