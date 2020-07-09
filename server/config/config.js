30125c474376e625f7f3787c1940200142d40197
Maxi Aquino <maxiaquino0@gmail.com>
2020-07-09 20:06:24 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://maxiaquino0:g3HMz4A9asAz6tm4@cluster0.zkypo.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
